d566ed25f679538a240d5b9e9386584a6a363bac
jkallson <janar.kallson@gmail.com>
2019-03-21 18:45:02 +0200

    Tegin faili sisselugemise korda. Lisasin tiimide algviisiku ning vahetusmängijate loomise süsteemi.

--- Basketball Project/src/failiSisseLugeja.java
+++ Basketball Project/src/failiSisseLugeja.java
@@ -5,7 +5,20 @@ import java.util.Scanner;
 public class failiSisseLugeja {
     private String tiim1;
     private String tiim2;
+    //listid, kus hoian kõiki andmeid mängijate kohta
     private ArrayList<String> tiim1List = new ArrayList<>();
+    private ArrayList<String> tiim2List = new ArrayList<>();
+    //listid, kus hoian ainult mängijate nimesid, et kasutaja saaks valida algviisiku jne
+    private ArrayList<String> tiim1MängijateList = new ArrayList<>();
+    private ArrayList<String> tiim2MängijateList = new ArrayList<>();
+
+    public ArrayList<String> getTiim1MängijateList() {
+        return tiim1MängijateList;
+    }
+
+    public ArrayList<String> getTiim2MängijateList() {
+        return tiim2MängijateList;
+    }
 
     public ArrayList<String> getTiim1List() {
         return tiim1List;
@@ -15,7 +28,6 @@ public class failiSisseLugeja {
         return tiim2List;
     }
 
-    private ArrayList<String> tiim2List = new ArrayList<>();
 
     public failiSisseLugeja(String tiim1, String tiim2) {
         this.tiim1 = tiim1;
@@ -29,19 +41,22 @@ public class failiSisseLugeja {
         try (Scanner sc = new Scanner(fail, "UTF-8")) {
             while (sc.hasNextLine()) {
                 String rida = sc.nextLine();
-                String[] tükid = rida.split(" ");
+                String[] tükid = rida.split(",");
                 for (int i = 0; i < tükid.length; i++) {
                     tiim1List.add(tükid[i]);
                 }
+                tiim1MängijateList.add(tükid[0]);
             }
         }
+        //loon scanneri, mille abil loen teise tiimi mängijad järjendisse
         try (Scanner sc = new Scanner(fail2, "UTF-8")) {
             while (sc.hasNextLine()) {
                 String rida = sc.nextLine();
-                String[] tükid = rida.split(" ");
+                String[] tükid = rida.split(",");
                 for (int i = 0; i < tükid.length; i++) {
                     tiim2List.add(tükid[i]);
                 }
+                tiim2MängijateList.add(tükid[0]);
             }
         }
     }
